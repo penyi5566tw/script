ae9ae619f98ec27ff9e164950a202df1e18a698b
penyi5566 <penyi5566@gmail.com>
2026-07-05 15:15:50 +0800

    Add build_llvm.sh

---
+++ build_llvm.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+SRC_DIR=/root/llvm/llvm-project/llvm
+BUILD_DIR=/root/llvm/build
+
+mkdir -p "${BUILD_DIR}"
+
+cmake                                                   \
+  -S "${SRC_DIR}"                                       \
+  -B "${BUILD_DIR}"                                     \
+  -G Ninja                                              \
+  -DCMAKE_C_COMPILER=/usr/bin/x86_64-linux-gnu-gcc-11   \
+  -DCMAKE_CXX_COMPILER=/usr/bin/x86_64-linux-gnu-g++-11 \
+  -DCMAKE_BUILD_TYPE=RelWithDebInfo                     \
+  -DLLVM_ENABLE_PROJECTS="clang"                        \
+  -DLLVM_TARGETS_TO_BUILD="RISCV"                       \
+  -DLLVM_ENABLE_ASSERTIONS="true"                       \
+  -DLLVM_USE_LINKER=lld                                 \
+  -DLLVM_USE_SPLIT_DWARF=ON                             \
+  -DLLVM_LINK_LLVM_DYLIB=ON
+
+cmake --build "${BUILD_DIR}"
